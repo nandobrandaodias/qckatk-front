dadc0fea0115f8b971b4328cf692fbe59ef9baf2
Lucas Fernando <nando.brandao173@gmail.com>
2025-02-25 15:53:50 -0300

    fix: login interface

--- src/app/modules/landing-page/main/main.component.ts
+++ src/app/modules/landing-page/main/main.component.ts
@@ -1,25 +1,12 @@
-import { Component, EventEmitter, HostListener, Output, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/modules/shared.module';
-import { Popover } from 'primeng/popover';
-import { LoginComponent } from "./components/login/login.component";
+import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-main',
-  imports: [SharedModule, LoginComponent],
+  imports: [SharedModule, NavbarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
 export class MainComponent {
-  @Output() onBlur = new EventEmitter<void>();
-  @ViewChild('op') op!: Popover;
-  showLogin: boolean = false;
-
-  @HostListener('blur') // Escuta o evento de blur
-  onBlurHandler() {
-    this.onBlur.emit(); // Dispara o evento onBlur
-  }
-
-  toggleLogin() {
-    this.showLogin = !this.showLogin
-  }
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
+import { LoginComponent } from "../../../modules/landing-page/main/components/login/login.component";
 
 @Component({
   selector: 'app-navbar',
-  imports: [SharedModule],
+  imports: [SharedModule, LoginComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  showLogin: boolean = false;
+
+  toggleLogin() {
+    this.showLogin = !this.showLogin
+  }
 }
